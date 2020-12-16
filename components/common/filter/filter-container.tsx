b85b306e75b80e17212afebb6f963995889c263f
lbeltranc <60752209+lbeltranc@users.noreply.github.com>
2020-12-16 17:02:43 -0500

    added sections

--- components/common/filter/filter-container.tsx
+++ components/common/filter/filter-container.tsx
@@ -1,4 +1,6 @@
 import styles from './filter-container.module.css'
+import update from 'immutability-helper'
+import { useState, useEffect } from 'react'
 
 // Components
 import FilterSelector from './filter-selector'
@@ -6,11 +8,135 @@ import FilterSelector from './filter-selector'
 
 const FilterContainer = () => {
 
-    const tags = ['Q3 Creative', 'women', 'men', 'inside', 'spring', 'outdoor']
+    const [expandedMenus, setExpandedMenus] = useState(['tags', 'channels', 'campaigns'])
+
+    const tags = [
+
+        {
+            name: 'Women',
+            total: '25'
+        },
+        {
+            name: 'men',
+            total: '15'
+        },
+        {
+            name: 'fall',
+            total: '34'
+        },
+        {
+            name: 'winter',
+            total: '3'
+        },
+        {
+            name: 'summer',
+            total: '2'
+        },
+        {
+            name: 'spring',
+            total: '18'
+        },
+    ]
+    const channels = [
+
+        {
+            name: 'email',
+            total: '55'
+        },
+        {
+            name: 'Facebook',
+            total: '65'
+        },
+        {
+            name: 'Ads',
+            total: '2'
+        },
+        {
+            name: 'Twitter',
+            total: '78'
+        }
+    ]
+    const campaigns = [
+
+        {
+            name: 'black friday 2020',
+            total: '55'
+        },
+        {
+            name: 'valentines day 2020',
+            total: '65'
+        },
+        {
+            name: 'holiday 2020',
+            total: '2'
+        },
+        {
+            name: 'Twitter',
+            total: '78'
+        }
+    ]
+    const projects = [
+
+        {
+            name: 'project test 1',
+            total: '3'
+        },
+        {
+            name: 'project test 2',
+            total: '23'
+        },
+        {
+            name: 'project test 3',
+            total: '32'
+        },
+        {
+            name: 'Tproject test 4',
+            total: '44'
+        },
+        {
+            name: 'Tproject test 5',
+            total: '44'
+        },
+        {
+            name: 'Tproject test 6',
+            total: '44'
+        }
+    ]
+    const fileTypes = [
+        {
+            name: 'jpg',
+            total: '3'
+        },
+        {
+            name: 'pdf',
+            total: '23'
+        },
+        {
+            name: 'png',
+            total: '32'
+        },
+        {
+            name: 'doc',
+            total: '44'
+        },
+    ]
+
+    const handleExpand = (menu) => {
+        let index = expandedMenus.findIndex((item) => item === menu)
+        if(index !== -1 ){
+            setExpandedMenus(update(expandedMenus, {
+                $splice: [[index, 1]]
+            }))
+        }else {
+            setExpandedMenus(update(expandedMenus, {
+                $push: [menu]
+            }))
+        }
+    }
 
     return (
         <div className={`${styles.container}`}>
-           <section className={styles['top-bar']}>
+            <section className={styles['top-bar']}>
                 <h3>Filters</h3>
                 <div className={`${styles.clear}`}>
                     <p>Clear</p>
@@ -18,8 +144,67 @@ const FilterContainer = () => {
                 </div>
             </section>
             <section>
-                <h4>Tags</h4>
-                <FilterSelector />
+                <div className={styles['expand-bar']} onClick={() => handleExpand('tags')}>
+                    <h4>Tags</h4>
+                    {expandedMenus.includes('tags') ? <div className={styles['expand-icon']}>&#8743;</div> : <div className={styles['expand-icon']}>&#8744;</div>}
+                </div>
+                {expandedMenus.includes('tags') && <FilterSelector filters={tags} numItems={10} />}
+            </section>
+            <section>
+                <div className={styles['expand-bar']} onClick={() => handleExpand('channels')}>
+                    <h4>Channels</h4>
+                    {expandedMenus.includes('channels') ? <div className={styles['expand-icon']}>&#8743;</div> : <div className={styles['expand-icon']}>&#8744;</div>}
+                </div>
+                {expandedMenus.includes('channels') && <FilterSelector searchBar={false} filters={channels} numItems={8} />}
+            </section>
+            <section>
+                <div className={styles['expand-bar']} onClick={() => handleExpand('campaigns')}>
+                    <h4>Campaigns</h4>
+                    {expandedMenus.includes('campaigns') ? <div className={styles['expand-icon']}>&#8743;</div> : <div className={styles['expand-icon']}>&#8744;</div>}
+                </div>
+                {expandedMenus.includes('campaigns') && <FilterSelector filters={campaigns} oneColumn={true} numItems={5} />}
+            </section>
+            <section>
+                <div className={styles['expand-bar']} onClick={() => handleExpand('projects')}>
+                    <h4>Projects</h4>
+                    {expandedMenus.includes('projects') ? <div className={styles['expand-icon']}>&#8743;</div> : <div className={styles['expand-icon']}>&#8744;</div>}
+                </div>
+                {expandedMenus.includes('projects') && <FilterSelector filters={projects} oneColumn={true} numItems={5} />}
+            </section>
+            <section>
+                <div className={styles['expand-bar']} onClick={() => handleExpand('file-types')}>
+                    <h4>File Types</h4>
+                    {expandedMenus.includes('file-types') ? <div className={styles['expand-icon']}>&#8743;</div> : <div className={styles['expand-icon']}>&#8744;</div>}
+                </div>
+                {expandedMenus.includes('file-types') && <FilterSelector filters={fileTypes} numItems={5} />}
+            </section>
+            <section>
+                <div className={styles['expand-bar']} onClick={() => handleExpand('product')}>
+                    <h4>Product</h4>
+                    {expandedMenus.includes('product') ? <div className={styles['expand-icon']}>&#8743;</div> : <div className={styles['expand-icon']}>&#8744;</div>}
+                </div>
+                {expandedMenus.includes('product') && <FilterSelector filters={fileTypes} numItems={5} />}
+            </section>
+            <section>
+                <div className={styles['expand-bar']} onClick={() => handleExpand('date')}>
+                    <h4>Date Uploaded</h4>
+                    {expandedMenus.includes('date') ? <div className={styles['expand-icon']}>&#8743;</div> : <div className={styles['expand-icon']}>&#8744;</div>}
+                </div>
+                {expandedMenus.includes('date') && <FilterSelector filters={fileTypes} numItems={5} />}
+            </section>
+            <section>
+                <div className={styles['expand-bar']} onClick={() => handleExpand('orientation')}>
+                    <h4>Orientation</h4>
+                    {expandedMenus.includes('orientation') ? <div className={styles['expand-icon']}>&#8743;</div> : <div className={styles['expand-icon']}>&#8744;</div>}
+                </div>
+                {expandedMenus.includes('orientation') && <FilterSelector filters={fileTypes} numItems={5} />}
+            </section>
+            <section>
+                <div className={styles['expand-bar']} onClick={() => handleExpand('dimensions')}>
+                    <h4>Dimensions</h4>
+                    {expandedMenus.includes('dimensions') ? <div className={styles['expand-icon']}>&#8743;</div> : <div className={styles['expand-icon']}>&#8744;</div>}
+                </div>
+                {expandedMenus.includes('dimensions') && <FilterSelector filters={fileTypes} numItems={5} />}
             </section>
         </div>
 
--- components/common/filter/filter-selector.tsx
+++ components/common/filter/filter-selector.tsx
@@ -7,66 +7,39 @@ import IconClickable from '../buttons/icon-clickable'
 import Search from '../../common/inputs/search'
 
 
-const FilterSelector = () => {
+const FilterSelector = ({searchBar = true, filters, oneColumn = false, numItems}) => {
 
-    const [isSelected, setIsSelected] = useState(false)
+    const [isSelected, setIsSelected] = useState('')
 
-    const tags = [
-        {
-            name: 'Women',
-            total: '25'
-        },
-        {
-            name: 'men',
-            total: '15'
-        },
-        {
-            name: 'fall',
-            total: '34'
-        },
-        {
-            name: 'winter',
-            total: '3'
-        },
-        {
-            name: 'summer',
-            total: '2'
-        },
-        {
-            name: 'spring',
-            total: '18'
-        },
-    ]
-
-    const toggleSelected = () => {
-        if (!isSelected) {
-            setIsSelected(true)
+    const toggleSelected = (name) => {
+        if (isSelected === '') {
+            setIsSelected(name)
         } else {
-            setIsSelected(false)
+            setIsSelected('')
         }
     }
 
-
     return (
         <div className={`${styles.container}`}>
-            <ul className={styles['item-list']}>
-                {tags.map((tag, index) => (
+            <ul className={`${styles['item-list']} ${oneColumn && styles['one-column']}`}>
+                {filters.slice(0, numItems).map((filter, index) => (
                     <li key={index} className={styles['select-item']}>
                         <div className={`${styles['selectable-wrapper']} ${isSelected && styles['selected-wrapper']}`}>
                             {isSelected ?
-                                <IconClickable src={Utilities.radioButtonEnabled} additionalClass={styles['select-icon']} onClick={toggleSelected} />
+                                <IconClickable src={Utilities.radioButtonEnabled} additionalClass={styles['select-icon']} onClick={() => toggleSelected(filter.name)} />
                                 :
-                                <IconClickable src={Utilities.radioButtonNormal} additionalClass={styles['select-icon']} onClick={toggleSelected} />
+                                <IconClickable src={Utilities.radioButtonNormal} additionalClass={styles['select-icon']} onClick={() => toggleSelected(filter.name)} />
                             }
                         </div>
-                        <p className={styles['item-name']}>{tag.name}</p>
-                        <p className={styles['item-total']}>{tag.total}</p>
+                        <p className={styles['item-name']}>{filter.name}</p>
+                        <p className={styles['item-total']}>{filter.total}</p>
                     </li>
                 ))}
             </ul>
-            <Search
-                placeholder={'Search'}
-            />
+            {searchBar &&
+                <Search
+                    placeholder={'Search'}
+                />}
         </div>
 
     )
