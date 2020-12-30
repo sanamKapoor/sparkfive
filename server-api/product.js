d458640f4a51d8ce2cea9bfa05932bef14d2f751
Santiago <santibelca@gmail.com>
2020-12-30 14:38:01 -0500

    changed a couple fo routes

--- server-api/product.js
+++ server-api/product.js
@@ -6,5 +6,6 @@ const productUrl = `${process.env.SERVER_BASE_URL}/products`
 export default {
   getProducts: (queryParams) => axios.get(`${productUrl}?${querystring.stringify(queryParams)}`),
   patchProduct: (id, patchData) => axios.patch(`${productUrl}/${id}`, patchData),
-  addValue: (id, valueData) => axios.post(`${productUrl}/${id}/value`, valueData),
+  addTag: (id, tagData) => axios.post(`${productUrl}/${id}/tags`, tagData),
+  deleteTag: (id, tagId) => axios.post(`${productUrl}/${id}/tags/${tagId}`)
 }
