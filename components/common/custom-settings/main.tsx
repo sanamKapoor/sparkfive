db3cccaa105c6418369a889bc63ae231b0f2080a
hdminh247 <hdminh247@gmail.com>
2021-07-11 15:01:15 +0700

    Only allow super admin access to preset size configurations

--- components/common/custom-settings/main.tsx
+++ components/common/custom-settings/main.tsx
@@ -1,12 +1,18 @@
 import styles from './main.module.css'
-import { useState } from 'react'
+import {useContext, useState} from 'react'
 
 // Components
 import SectionButton from '../../common/buttons/section-button'
 import CustomFileSizes from "./custom-file-size"
 import SizeSaPresets from "./size-sa-presets"
+import { UserContext } from '../../../context'
+
+import {
+    SUPERADMIN_ACCESS
+} from '../../../constants/permissions'
 
 const Main = () => {
+    const { hasPermission } = useContext(UserContext)
     const [activeList, setActiveList] = useState('customFileSizes')
 
     return (
@@ -17,15 +23,15 @@ const Main = () => {
                     active={activeList === 'customFileSizes'}
                     onClick={() => setActiveList('customFileSizes')}
                 />
-                <SectionButton
+                {hasPermission([SUPERADMIN_ACCESS]) && <SectionButton
                     text='Size SA Presets'
                     active={activeList === 'sizeSaPresets'}
                     onClick={() => setActiveList('sizeSaPresets')}
-                />
+                />}
             </div>
 
             {activeList === 'customFileSizes' && <CustomFileSizes />}
-            {activeList === 'sizeSaPresets' && <SizeSaPresets />}
+            {activeList === 'sizeSaPresets' && hasPermission([SUPERADMIN_ACCESS]) && <SizeSaPresets />}
         </>
     )
 }
