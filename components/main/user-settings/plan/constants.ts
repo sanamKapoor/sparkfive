fdba302281405247ada3783c34fd42acc2f15ed4
hdminh247 <hdminh247@gmail.com>
2021-09-28 10:28:19 +0700

    Fix premium plus plan is not defined

--- components/main/user-settings/plan/constants.ts
+++ components/main/user-settings/plan/constants.ts
@@ -1,9 +1,11 @@
 const PRO_V1_SUMMARY = 'Combines digital asset management with a robust market management platform'
 const PREMIUM_V1_SUMMARY = 'Ideal for teams that are scaling and have more complex workflows'
+const PREMIUM_PLUS_V1_SUMMARY = 'Ideal for teams that are scaling and have more complex workflows'
 const ENTERPRISE_SUMMARY = 'Robust Enterprise capabilities to serve larger companies'
 
 const DAM_PRO_V1_SUMMARY = 'Perfect for smaller teams poised for growth'
 const DAM_PREMIUM_V1_SUMMARY = 'Ideal for teams that are scaling and have more complex workflows'
+const DAM_PREMIUM_PLUS_V1_SUMMARY = 'Ideal for teams that are scaling and have more complex workflows'
 const DAM_ENTERPRISE_SUMMARY = 'Robust Enterprise capabilities to serve larger companies'
 
 const PRO_V1_FEATURES = [
@@ -24,6 +26,15 @@ const PREMIUM_V1_FEATURES = [
   'Advanced Integrations'
 ]
 
+const PREMIUM_PLUS_V1_FEATURES = [
+  'Everything from PRO',
+  '1TB of Storage',
+  '3 Admins',
+  'Advanced Workflow tools',
+  'Asset distribution via CDN',
+  'Advanced Integrations'
+]
+
 const ENTERPRISE_FEATURES = [
   'Everything from PREMIUM',
   'Custom Storage',
@@ -36,7 +47,7 @@ const ENTERPRISE_FEATURES = [
 const DAM_PRO_V1_FEATURES = [
   '100GB of Storage',
   '1 Admin',
-  'Unlimited Users',  
+  'Unlimited Users',
   'Collaboration Tools',
   'Access Controls'
 ]
@@ -45,6 +56,15 @@ const DAM_PREMIUM_V1_FEATURES = [
   'Everything from PRO',
   '1TB of Storage',
   '3 Admins',
+  'Advanced Workflow tools',
+  'Asset distribution via CDN',
+  'Advanced Integrations'
+]
+
+const DAM_PREMIUM_PLUS_V1_FEATURES = [
+  'Everything from PRO',
+  '1TB of Storage',
+  '8 Admins',
   'Advanced Workflow tools',
   'Asset distribution via CDN',
   'Advanced Integrations'
@@ -73,6 +93,9 @@ export default {
   PREMIUM_V1_SUMMARY,
   PREMIUM_V1_FEATURES,
 
+  PREMIUM_PLUS_V1_SUMMARY,
+  PREMIUM_PLUS_V1_FEATURES,
+
   ENTERPRISE_SUMMARY,
   ENTERPRISE_FEATURES,
 
@@ -81,6 +104,9 @@ export default {
 
   DAM_PREMIUM_V1_FEATURES,
   DAM_PREMIUM_V1_SUMMARY,
+
+  DAM_PREMIUM_PLUS_V1_FEATURES,
+  DAM_PREMIUM_PLUS_V1_SUMMARY,
 
   DAM_ENTERPRISE_FEATURES,
   DAM_ENTERPRISE_SUMMARY,
