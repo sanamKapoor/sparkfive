4501388401d0489e781a9b6e24a531b6d71c2f93
hdminh247 <hdminh247@gmail.com>
2021-11-24 09:34:00 +0700

    Add some test pages

---
+++ pages/main/user-settings/[view]/manage.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+
+import FilterProvider from '../../../../context/filter-provider'
+import { ASSET_ACCESS } from '../../../../constants/permissions'
+
+// Components
+import MainLayout from '../../../../components/common/layouts/main-layout'
+
+import DeletedAssetsLibrary from '../../../../components/common/custom-settings/deleted-assets'
+import { useContext } from 'react'
+import { UserContext } from '../../../../context'
+import NoPermissionNotice from '../../../../components/common/misc/no-permission-notice'
+
+const AssetsPage = () => {
+
+    const {user} = useContext(UserContext)
+    return <FilterProvider>
+        <Head>
+            <title>User Settings</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <MainLayout requiredPermissions={[ASSET_ACCESS]}>
+            {
+                (user.roleId === 'admin' || user.roleId === 'super_admin') ? <DeletedAssetsLibrary /> : <NoPermissionNotice />
+            }
+        </MainLayout>
+    </FilterProvider>
+}
+
+export default AssetsPage
