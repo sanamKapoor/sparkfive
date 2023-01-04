27dfca47af117517ab2274279a6f408dc175c4e1
Saravanan <ksarav89@gmail.com>
2023-01-04 18:30:37 +0530

    SSO login code changes

--- components/common/containers/providers-auth.tsx
+++ components/common/containers/providers-auth.tsx
@@ -26,11 +26,18 @@ let ProvidersAuth = ({ inviteCode = '', priceData }) => {
     <div className={styles.container}>
       <div>
         <ProviderAuthButton
+          provider='sso'
+          icon=''
+          onClick={() => initiateOAuth('okta')}
+        />
+      </div>
+      {/* <div>
+        <ProviderAuthButton
           provider='google'
           icon={LoginImg.google}
           onClick={() => initiateOAuth('google')}
         />
-      </div>
+      </div> */}
     </div>
   )
 }
