5e1f4989d6345ab255062197dd82661be568dac8
Antra Verma <>
2023-01-31 14:59:10 +0530

    added download function and ui for accounts

--- constants/messages.ts
+++ constants/messages.ts
@@ -2,9 +2,11 @@
 export const ASSET_NAME_UPDATED = "Asset name updated.";
 export const COLLECTION_NAME_UPDATED = "Collection name updated.";
 export const USERS_DOWNLOADED = "Users list downloaded.";
+export const ACCOUNTS_DOWNLOADED = "Accounts list downloaded.";
 
 //errors
 export const FAILED_TO_UPDATE_ASSET_NAME = "Failed to update asset name.";
 export const FAILED_TO_UPDATE_COLLECTION_NAME =
   "Failed to update collection name.";
 export const FAILED_TO_DOWNLOAD_USERS = "Failed to download users list.";
+export const FAILED_TO_DOWNLOAD_ACCOUNTS = "Failed to download accounts list.";
