580b9023905f14721c5feae8c09dcbe19d193d2a
Gaurav Bajaj <>
2023-02-27 15:19:00 +0530

    env changes

--- next.config.js
+++ next.config.js
@@ -22,7 +22,8 @@ module.exports = withSentryConfig(withImages({
     SOCKET_BASE_URL: process.env.SERVER_BASE_URL ? `${process.env.SERVER_BASE_URL}/` : "http://localhost:8080/",
     CLIENT_BASE_URL: process.env.CLIENT_BASE_URL || "http://localhost:3000",
     // SERVER_BASE_URL: "https://d13c3bdc9413.ngrok.io",
-    DROPBOX_API_KEY: process.env.DROPBOX_API_KEY || "gtwo80vc34l8vjd",
+    // DROPBOX_API_KEY: process.env.DROPBOX_API_KEY || "gtwo80vc34l8vjd",
+    DROPBOX_API_KEY: process.env.DROPBOX_API_KEY || "6rimytfrzluesfn",
     STRIPE_PUBLIC_KEY:
       process.env.STRIPE_PUBLIC_KEY || "pk_test_bK1C20PBomU24spmlMeg4AXp",
     STRIPE_EXPIRE_PRODUCT_NAME:
@@ -33,11 +34,11 @@ module.exports = withSentryConfig(withImages({
     DEFAULT_TRIAL_PRODUCT:
       process.env.DEFAULT_TRIAL_PRODUCT || "prod_Hv5C1USjYMtYBp",
     GOOGLE_CLIENT_ID:
-      process.env.GOOGLE_CLIENT_ID ||
-      "1053631313639-o0m00gdem0cgd3agg6i4o0iop657llkk.apps.googleusercontent.com",
+      process.env.GOOGLE_CLIENT_ID || "1003078561600-mf9f0cb0303di0ps9u8n3tgqvoddl7c6.apps.googleusercontent.com",
+      //"1053631313639-o0m00gdem0cgd3agg6i4o0iop657llkk.apps.googleusercontent.com",
     GOOGLE_DEVELOPER_KEY:
-      process.env.GOOGLE_DEVELOPER_KEY ||
-      "AIzaSyAqsbbj0ufdPdUO7tQwkvU1gAPn19hTo3s",
+      process.env.GOOGLE_DEVELOPER_KEY || "AIzaSyAYOOdG3qKk5ZSwoI-8WtXepioT1SdduyA",
+      //"AIzaSyAqsbbj0ufdPdUO7tQwkvU1gAPn19hTo3s",
     APPSYNC_GRAPHQL_REALTIMEURL:
       process.env.APPSYNC_GRAPHQL_REALTIMEURL ||
       "wss://it7l2l7v25dvhhet4izohabywu.appsync-realtime-api.us-east-1.amazonaws.com/graphql",
