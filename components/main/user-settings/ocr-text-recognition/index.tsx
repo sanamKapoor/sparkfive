1ddb404d73ba4b279d560ba6f0c8d5bb2476d84f
hdminh247 <hdminh247@gmail.com>
2023-05-23 20:03:01 +0700

    Limit OCR by setting

--- components/main/user-settings/ocr-text-recognition/index.tsx
+++ components/main/user-settings/ocr-text-recognition/index.tsx
@@ -1,12 +1,22 @@
+import {useContext} from "react";
+
 import styles from './index.module.css'
+
+
 
 // Component
 import Main from "../../../common/ocr-text-recognition/main";
+import NoPermissionNotice from "../../../common/misc/no-permission-notice";
+
+
+import { UserContext } from '../../../../context'
 
 const OCR = () => {
+    const { user } = useContext(UserContext)
   return (
     <div className={styles.container}>
-      <Main />
+        {!user?.team?.ocr && <NoPermissionNotice noPaddingLeft={true}/>}
+        {user?.team?.ocr && <Main />}
     </div>
   )
 }
