ce47a4ac6291225d43ddae116bdcf7ffccc5bdc3
Antra Verma <webdev2@netaesthics.com>
2023-09-26 11:35:49 +0530

    fixed account action options

--- components/common/custom-settings/account-actions.tsx
+++ components/common/custom-settings/account-actions.tsx
@@ -9,7 +9,6 @@ const AccountActions = () => {
   const [loading, setLoading] = useState(false);
   const { advancedConfig, setAdvancedConfig } = useContext(UserContext);
 
-
   const saveAdvanceConfig = async (config) => {
     try {
       setLoading(true);
@@ -17,116 +16,118 @@ const AccountActions = () => {
 
       const updatedConfig = { ...advancedConfig, ...config };
       setAdvancedConfig(updatedConfig);
-
     } catch (err) {
-      console.log('err in saving advanced config: ', err)
+      console.log("err in saving advanced config: ", err);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   };
 
-
-
   return (
     <div className={styles.container}>
-      <div className={`${styles['custom-view-wrapper']}`}>
-      <h3>Account Actions</h3>
+      <div className={`${styles["custom-view-wrapper"]}`}>
+        <h3>Account Actions</h3>
 
-      <div>
-        <div className={styles.row}>
-          <span className={styles.label}>Search Default</span>
-          <div className={styles["field-radio-wrapper"]}>
-            <div className={`${styles['radio']} ${styles['account-action-wrapper']}`}>
-              <div>Subfolder as Separate Collection</div>
-              <IconClickable
-                src={
-                  advancedConfig?.searchDefault === "all"
-                    ? Utilities.radioButtonEnabled
-                    : Utilities.radioButtonNormal
-                }
-                additionalClass={styles["select-icon"]}
-                onClick={() => saveAdvanceConfig({ searchDefault: "all" })}
-              />
+        <div>
+          <div className={styles.row}>
+            <div className={styles.label}>Folder Upload Configuration</div>
+            <div className={styles["field-radio-wrapper"]}>
+              <div
+                className={`${styles["radio"]} ${styles["account-action-wrapper"]}`}
+              >
+                <div>Subfolder as Separate Collection</div>
+                <IconClickable
+                  src={
+                    advancedConfig?.subFolderAutoTag
+                      ? Utilities.radioButtonEnabled
+                      : Utilities.radioButtonNormal
+                  }
+                  additionalClass={styles["select-icon"]}
+                  onClick={() => saveAdvanceConfig({ subFolderAutoTag: true })}
+                />
+              </div>
+              <div className={`${styles["radio"]} ${styles["subfolder-tags"]}`}>
+                <div>Subfolder as Tags (Default)</div>
+                <IconClickable
+                  src={
+                    !advancedConfig?.subFolderAutoTag
+                      ? Utilities.radioButtonEnabled
+                      : Utilities.radioButtonNormal
+                  }
+                  additionalClass={styles["select-icon"]}
+                  onClick={() => saveAdvanceConfig({ subFolderAutoTag: false })}
+                />
+              </div>
             </div>
-            <div className={`${styles['radio']} ${styles['subfolder-tags']}`}>
-              <div>Subfolder as Tags (Default)</div>
-              <IconClickable
-                src={
-                  advancedConfig?.searchDefault === "tags_only"
-                    ? Utilities.radioButtonEnabled
-                    : Utilities.radioButtonNormal
-                }
-                additionalClass={styles["select-icon"]}
-                onClick={() =>
-                  saveAdvanceConfig({ searchDefault: "tags_only" })
-                }
-              />
+          </div>
+          <div className={styles.row}>
+            <span className={styles.label}>Search Default</span>
+            <div className={styles["field-radio-wrapper"]}>
+              <div
+                className={`${styles["radio"]} ${styles["account-action-wrapper"]}`}
+              >
+                <div>All</div>
+                <IconClickable
+                  src={
+                    advancedConfig?.searchDefault === "all"
+                      ? Utilities.radioButtonEnabled
+                      : Utilities.radioButtonNormal
+                  }
+                  additionalClass={styles["select-icon"]}
+                  onClick={() => saveAdvanceConfig({ searchDefault: "all" })}
+                />
+              </div>
+              <div className={`${styles["radio"]} ${styles["subfolder-tags"]}`}>
+                <div>Tags Only</div>
+                <IconClickable
+                  src={
+                    advancedConfig?.searchDefault === "tags_only"
+                      ? Utilities.radioButtonEnabled
+                      : Utilities.radioButtonNormal
+                  }
+                  additionalClass={styles["select-icon"]}
+                  onClick={() =>
+                    saveAdvanceConfig({ searchDefault: "tags_only" })
+                  }
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.row}>
+            <div className={styles.label}>
+              Duplicate Management - Check Uploads
+            </div>
+            <div className={styles["field-radio-wrapper"]}>
+              <div
+                className={`${styles["radio"]} ${styles["account-action-wrapper"]}`}
+              >
+                <div>On</div>
+                <IconClickable
+                  src={
+                    advancedConfig?.duplicateCheck
+                      ? Utilities.radioButtonEnabled
+                      : Utilities.radioButtonNormal
+                  }
+                  additionalClass={styles["select-icon"]}
+                  onClick={() => saveAdvanceConfig({ duplicateCheck: true })}
+                />
+              </div>
+              <div className={`${styles["radio"]} ${styles["subfolder-tags"]}`}>
+                <div>Off</div>
+                <IconClickable
+                  src={
+                    !advancedConfig?.duplicateCheck
+                      ? Utilities.radioButtonEnabled
+                      : Utilities.radioButtonNormal
+                  }
+                  additionalClass={styles["select-icon"]}
+                  onClick={() => saveAdvanceConfig({ duplicateCheck: false })}
+                />
+              </div>
             </div>
           </div>
         </div>
-
-        <div className={styles.row}>
-          <div className={styles.label}>Folder Upload Configuration</div>
-          <div className={styles["field-radio-wrapper"]}>
-          <div className={`${styles['radio']} ${styles['account-action-wrapper']}`}>
-              <div>All</div>
-              <IconClickable
-                src={
-                  advancedConfig?.subFolderAutoTag
-                    ? Utilities.radioButtonEnabled
-                    : Utilities.radioButtonNormal
-                }
-                additionalClass={styles["select-icon"]}
-                onClick={() => saveAdvanceConfig({ subFolderAutoTag: true })}
-              />
-            </div>
-            <div className={`${styles['radio']} ${styles['subfolder-tags']}`}>
-              <div>Tags Only</div>
-              <IconClickable
-                src={
-                  !advancedConfig?.subFolderAutoTag
-                    ? Utilities.radioButtonEnabled
-                    : Utilities.radioButtonNormal
-                }
-                additionalClass={styles["select-icon"]}
-                onClick={() => saveAdvanceConfig({ subFolderAutoTag: false })}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.row}>
-          <div className={styles.label}>
-            Duplicate Management - Check Uploads
-          </div>
-          <div className={styles["field-radio-wrapper"]}>
-          <div className={`${styles['radio']} ${styles['account-action-wrapper']}`}>
-              <div>On</div>
-              <IconClickable
-                src={
-                  advancedConfig?.duplicateCheck
-                    ? Utilities.radioButtonEnabled
-                    : Utilities.radioButtonNormal
-                }
-                additionalClass={styles["select-icon"]}
-                onClick={() => saveAdvanceConfig({ duplicateCheck: true })}
-              />
-            </div>
-            <div className={`${styles['radio']} ${styles['subfolder-tags']}`}>
-              <div>Off</div>
-              <IconClickable
-                src={
-                  !advancedConfig?.duplicateCheck
-                    ? Utilities.radioButtonEnabled
-                    : Utilities.radioButtonNormal
-                }
-                additionalClass={styles["select-icon"]}
-                onClick={() => saveAdvanceConfig({ duplicateCheck: false })}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
       </div>
     </div>
   );
