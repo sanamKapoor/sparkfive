5c3f8492b18d8a53e8403fb42c3e1ee3878edd3f
nitin jain <nitin@netaesthetics.com>
2023-12-19 10:24:10 +0530

    ui of user insight

--- components/analytics/account.tsx
+++ components/analytics/account.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import TableComponent from './insight-table/insight-table'
 
 export default function Account() {
-    const columns = ["Username", "Email", "Role", "Actions"];
+    const columns = ["Username", "Role","Last session date","Sessions","Downloads","Shares", "Actions",];
     const data = [
-      { Username: "John Doe", Email: "john@example.com", Role: "Admin", Actions: "Edit" },
-      { Username: "Jane Smith", Email: "jane@example.com", Role: "User", Actions: "Delete" },
-      // Add more rows as needed
+      { Username: "John Doe",  Role: "Admin", "Last session date": "Today at 04:22pm", Sessions:"1.27",Downloads:"77",Shares:"30", Actions: "Edit" ,},
+      { Username: "Jane Smith",  Role: "User", "Last session date": "Today at 04:01pm", Sessions:"93",Downloads:"77",Shares:"30",  Actions: "Delete" },
+      
     ];
   
-    const arrowColumns = ["Username", "Role"]; // Columns where you want to display arrow icons
-    const buttonColumns = ["Actions"]; // Columns where you want to display buttons
+    const arrowColumns = ["Username", "Role","Last session date","Sessions","Downloads","Shares"];
+    const buttonColumns = ["Actions"]; 
     const buttonTexts = { Actions: "User Info" };
   return (
     <div>
