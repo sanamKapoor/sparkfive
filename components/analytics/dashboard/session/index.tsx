66207e814fb2ccbacbff19552ab78bf9fc7ec97d
nitin jain <nitin@netaesthetics.com>
2024-02-02 14:24:28 +0530

    ui changes in responsive

--- components/analytics/dashboard/session/index.tsx
+++ components/analytics/dashboard/session/index.tsx
@@ -22,7 +22,7 @@ const TeamSession = ({ initialData }) => {
 
 
   return (
-    <div className={styles.tableResponsive}>
+    <div className={`${styles["tableResponsive"]} ${styles["dashboard-chart"]}`}>
       <div className={styles.headerContainer}>
         <Heading mainText="Top Team Session" smallHeading={true} />
         <div className={`${styles["table-header-tabs"]}`}>
